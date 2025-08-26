5c042694a0ffcef27a6676e692d3721da029e76b
Phan Tien Lam Thong <max.phan@endava.com>
2025-08-26 21:54:12 +0700

    edit something

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
 output "ecr_repo_url" {
-  value = 507737351904.dkr.ecr.ap-southeast-1.amazonaws.com/devsecops-demo
+  value = "507737351904.dkr.ecr.ap-southeast-1.amazonaws.com/devsecops-demo"
 }
 
 output "apprunner_service_url" {
-  value = epp74bjecz.ap-southeast-1.awsapprunner.com
+  value = "epp74bjecz.ap-southeast-1.awsapprunner.com"
 }
 
 output "github_actions_role_arn" {
-  value = arn:aws:iam::507737351904:role/devsecops-demo-github-actions-role
+  value = "arn:aws:iam::507737351904:role/devsecops-demo-github-actions-role"
 }
 
 output "apprunner_service_arn" {
-  value = arn:aws:apprunner:ap-southeast-1:507737351904:service/devsecops-demo/23eb5226c8894e74ad68633ab6a863f0
+  value = "arn:aws:apprunner:ap-southeast-1:507737351904:service/devsecops-demo/23eb5226c8894e74ad68633ab6a863f0"
 }
